a661d72280f26b10d1325f33d1a73cf8e2816e21
Winnie Bosibori <winniebosy@gmail.com>
2022-11-24 16:34:17 +0300

    main function to run the application

--- script.js
+++ script.js
@@ -1,94 +1,139 @@
-//addition
-let numInput = [1, 200, 5];
+//main container for calculator
 
-//addition
-function addNumbers(a, b) {
-    return a + b;
+//runs the whole application
+
+//addtion
+function addNums(value) {
+
 }
-const x = numInput.reduce(addNumbers)
-    //console.log(x)
-
-
 //subtraction
-function minusNumbers(a, b) {
-    return a - b;
-}
-const sub = numInput.reduce(minusNumbers)
-    // console.log(sub)
+function minusNums(value) {
 
+}
+//division
+function divideNums(value) {
+
+}
 //multiplication
-
-function multiplyNumbers(a, b) {
-    return a * b;
-}
-const times = numInput.reduce(multiplyNumbers)
-    // console.log(times)
-
-function divideNumbers(a, b) {
-    return (a / b).toFixed(5);
-}
-const divide = numInput.reduce(divideNumbers)
-    // console.log(divide)
-
-
-//functions to operate
-function operate(operator, num1, num2) {
-    switch (operator) {
-        case "+":
-            return addNumbers(num1, num2)
-        case "-":
-            return minusNumbers(num1, num2)
-        case "*":
-            return multiplyNumbers(num1, num2)
-        case "/":
-            return divideNumbers(num1, num2)
-    }
+function multiplyNums(value) {
 
 }
 
-//number buttons
-let numberBtns = document.querySelectorAll('.btn-number')
-let operatorBtns = document.querySelectorAll('.btn-operator')
-let onBtn = document.querySelector('.btn-onn')
-let offBtn = document.querySelector('.btn-offf')
-let delBtn = document.querySelector('.btn-delete')
-let clearBtn = document.querySelector('.btn-ac')
-let storedValue = ' ';
-let currentOperand = document.querySelector('.current-operand')
-let previousOperand = document.querySelector('.previous-operand');
-let clickedOperator = ''
-let firstNumber = '';
-let result = '';
-
-currentOperand.textContent = 0;
-previousOperand.textContent;
 
 
 
-numberBtns.forEach((numBtn) => {
-    numBtn.addEventListener('click', (e) => {
-        storedValue += numBtn.value;
-        previousOperand.textContent += storedValue;
-        console.log(numBtn.value)
-    })
-})
+function init() {
+    let calculator = document.querySelector('.calculator-app');
+    calculator.addEventListener('click', (e) => {
+        console.log(e.target.textContent)
+    });
+}
+
+init();
 
 
-//resets the calculator
-clearBtn.addEventListener('click', (e) => {
-    location.reload();
-})
 
-//switches off the calcualator
-offBtn.addEventListener('click', (e) => {
-    numberBtns.forEach((numBtn) => {
-        numBtn.value = ' '
-        currentOperand.textContent = ' '
-    })
-})
 
-//switch on calculator
-onBtn.addEventListener('click', (e) => {
-    currentOperand.textContent = 0;
-    location.reload();
-})
+
+
+
+
+
+// //addition
+// let numInput = [1, 200, 5];
+
+// //addition
+// function addNumbers(a, b) {
+//     return a + b;
+// }
+// const x = numInput.reduce(addNumbers)
+//     //console.log(x)
+
+
+// //subtraction
+// function minusNumbers(a, b) {
+//     return a - b;
+// }
+// const sub = numInput.reduce(minusNumbers)
+//     // console.log(sub)
+
+// //multiplication
+
+// function multiplyNumbers(a, b) {
+//     return a * b;
+// }
+// const times = numInput.reduce(multiplyNumbers)
+//     // console.log(times)
+
+// function divideNumbers(a, b) {
+//     return (a / b).toFixed(5);
+// }
+// const divide = numInput.reduce(divideNumbers)
+//     // console.log(divide)
+
+
+// //functions to operate
+// function operate(operator, num1, num2) {
+//     switch (operator) {
+//         case "+":
+//             return addNumbers(num1, num2)
+//         case "-":
+//             return minusNumbers(num1, num2)
+//         case "*":
+//             return multiplyNumbers(num1, num2)
+//         case "/":
+//             return divideNumbers(num1, num2)
+//     }
+
+// }
+
+// //number buttons
+// let numberBtns = document.querySelectorAll('.btn-number')
+// let operatorBtns = document.querySelectorAll('.btn-operator')
+// let onBtn = document.querySelector('.btn-onn')
+// let offBtn = document.querySelector('.btn-offf')
+// let delBtn = document.querySelector('.btn-delete')
+// let clearBtn = document.querySelector('.btn-ac')
+// let storedValue = ' ';
+// let currentOperand = document.querySelector('.current-operand')
+// let previousOperand = document.querySelector('.previous-operand');
+// let clickedOperator = ''
+// let firstNumber = '';
+// let result = '';
+
+// currentOperand.textContent = 0;
+// previousOperand.textContent;
+
+
+
+// numberBtns.forEach((numBtn) => {
+//     numBtn.addEventListener('click', (e) => {
+//         storedValue += numBtn.value;
+//         previousOperand.textContent += storedValue;
+//         console.log(numBtn.value)
+//     })
+// })
+
+
+// //resets the calculator
+// clearBtn.addEventListener('click', (e) => {
+//     location.reload();
+// })
+
+// //switches off the calcualator
+// offBtn.addEventListener('click', (e) => {
+//     numberBtns.forEach((numBtn) => {
+//         numBtn.value = ' '
+//         currentOperand.textContent = ' '
+//     })
+// })
+
+// //switch on calculator
+// onBtn.addEventListener('click', (e) => {
+//     currentOperand.textContent = 0;
+//     location.reload();
+// })
+
+
+
+// // https://stackoverflow.com/questions/63058457/calculator-project-stuck-javascript-reference
