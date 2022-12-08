e55a646d0b36cf2d6f80e069ab91ca35e70b899a
Winnie Bosibori <winniebosy@gmail.com>
2022-12-08 22:46:33 +0300

    incorporating keyboard accessibility

--- script.js
+++ script.js
@@ -5,40 +5,157 @@ let buffer = '0';
 //previous operator clicked
 let previousOperator;
 
-//screen
+//screen display
 const screen = document.querySelector('.current-operand');
+// let buttons
 
+//
 
-//handle button clicks
-function buttonClick(value) {
-    if (isNaN(value)) {
-        //this is not a number
-    } else {
-        //this is a number
-    }
+//CONTINUE FROM HERE!!
+//function to handle symbols
+function handleSymbols(symbol) {
+    if (symbol === 'AC') {
+        buffer = '0';
+        runningTotal = 0;
+    } else if (symbol === 27 || symbol === 'Escape') {
+        buffer = '0';
+        runningTotal = 0;
+
+    } else if
+
 
 }
 
+// switch (symbol) {
+//     case 'AC':
+//         if (e.key === 27 || e.key === 'Escape')
+//             buffer = '0';
+//         runningTotal = 0;
+//         break;
+//     case 'DEL':
+//         if (buffer.length === 1) {
+//             buffer = '0';
+//         } else {
+//             buffer = buffer.slice(0, buffer.length - 1);
+//         }
+//         break;
+//     case '✕':
+//     case '+':
 
-//function to hand symbols
-function handleSymbols() {}
+//     case '-':
+//     case '/':
+//     case '%':
+//     case '⋀':
+//         handleMath(symbol);
+//         break;
+
+// }
+
+
+
+//handling running totals
+function handleMath(symbol) {
+    console.log('handleMath', symbol)
+    if (buffer === '0') {
+        //do nothing
+        return;
+    }
+    //turn buffer to number
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        //keeps track of the previous operation
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer)
+    }
+    //keep track of previous operator for operating more than two numbers
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+//handle math
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '✕') {
+        runningTotal *= intBuffer;
+        //console.log(runningTotal)
+    } else if (previousOperator === '+') {
+        runningTotal += intBuffer
+            //console.log(runningTotal)
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer;
+        //console.log(runningTotal)
+    } else if (previousOperator === '/') {
+        runningTotal /= intBuffer;
+        //console.log(runningTotal)
+    } else if (previousOperator === '%') {
+        runningTotal = 0
+            // console.log(runningTotal)
+    } else if (previousOperator === '⋀') {
+        runningTotal **= intBuffer;
+        // console.log(runningTotal)
+    } else {
+        runningTotal = 0;
+        // console.log(runningTotal)
+    }
+
+}
+//keyboard handlers for numbers
+
 
 
 
 //function to handle numbers
-function handleNumbers() {}
+function handleNumbers(numberString) {
+    //numbers but still in string from
+    if (buffer === '0') {
 
+        buffer = numberString;
+    } else {
+        buffer = buffer + numberString;
+    }
+}
+
+//function handle keyboard keys
+function keyPress(value) {
+    if (isFinite(value)) {
+        handleNumbers(value)
+    } else {
+        handleSymbols(value)
+    }
+    screen.textContent = buffer;
+}
+
+
+
+//handle button clicks
+function buttonClick(value) {
+    if (!isNaN(value)) {
+        handleNumbers(value)
+    } else {
+        handleSymbols(value)
+    }
+    screen.textContent = buffer;
+}
 
 //init function
 function init() {
-    let buttons = document.querySelectorAll('.btn');
-    buttons.forEach((button) => {
+    document.querySelectorAll('button').forEach((button) => {
         button.addEventListener('click', (e) => {
-            console.log(e.target.textContent)
-            buttonClick(e.target.textContent)
+            let buttonText = e.target.textContent;
+            //console.log(buttonText)
+            buttonClick(buttonText)
         })
-    })
+
+    });
+
+    document.addEventListener('keydown', (e) => {
+        keyPress(e.key)
+        console.log(e.key)
+
+    });
 
 }
+
 
 init()
